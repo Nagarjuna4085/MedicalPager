25e54f437ca4244977d0cb591b5ba411f2d32ba7
nagarjnasanam <snagarjuna2001@gmail.com>
2025-10-23 08:21:28 -0400

    side bar icons

--- client/src/Sidebar/Sidebar.jsx
+++ client/src/Sidebar/Sidebar.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import {
+  Menu,
+  MessageCircleMore,
+  ChartPie,
+  PhoneCall,
+  Star,
+  Archive,
+  Settings,
+  CircleUserRound,
+} from "lucide-react";
 
 const Sidebar = () => {
+  const iconsTop = [Menu, PhoneCall, ChartPie];
+  const iconsBottom = [Star, Archive, Settings, CircleUserRound];
   return (
-    <div className="flex flex-col items-center justify-center gap-4 py-4">
-      <button className="p-2 hover:bg-gray-200 rounded">Chat</button>
-      <button className="p-2 hover:bg-gray-200 rounded">Chat</button>
-      <button className="p-2 hover:bg-gray-200 rounded">Chat</button>
+    <div className="flex flex-col items-center justify-between h-screen bg-primary text-muted-foreground py-4 ">
+      <div className="flex flex-col gap-6">
+        {iconsTop.map((Icon, key) => (
+          <Icon
+            key={key}
+            className="size-6 hover-sidebar-icons cursor-pointer hover:text-secondary"
+          />
+        ))}
+      </div>
+      <div className="flex flex-col gap-6 ">
+        {iconsBottom.map((Icon, key) => (
+          <Icon
+            key={key}
+            className="size-6 hover-sidebar-icons cursor-pointer hover:text-secondary"
+          />
+        ))}
+      </div>
     </div>
   );
 };
